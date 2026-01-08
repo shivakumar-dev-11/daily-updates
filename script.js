38bfdd19aad36a4457bb7c2b16d7224c565f10c1
shivakumar-dev-11 <bashamonishivakumar081@gmail.com>
2026-01-08 22:22:02 +0530

    second commit

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ let x = 5;
 let y = 10;
 let sum = x + y;
 console.log("the sum of x and y is " + sum);
+
+//push test examples
+let fruits = ["apple", "banana"];
+fruits.push("orange");
+console.log(fruits);
+
+let cars = ["bmw", "toyota"];
+cars.push("audi");
+console.log(cars);
+
+//pop test examples
+let fruit = ["apple", "banana ", "orange "];
+fruits.pop();
+console.log(fruits);
+
+let vehicles = ["bmw", "toyota", "audi"];
+vehicles.pop();
+console.log(vehicles);
