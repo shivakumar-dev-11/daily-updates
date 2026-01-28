8edf6ecddd273306000e16a579130ed40f0d16de
Shivakumar <bashamonishivakumar081@gmail.com>
2026-01-28 22:15:38 +0530

    last commit

--- problems.js
+++ problems.js
@@ -2,5 +2,9 @@ function submitForm() {
   const name = document.getElementById("name").value;
   const surname = document.getElementById("surname").value;
   const fullName = name + " " + surname;
+
   document.getElementById("output").innerText = " welcome  " + fullName;
+  const word = document.getElementById("word").value;
+  document.getElementById("wordLength").innerText =
+    "Length of the word is: " + word.length;
 }
