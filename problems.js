daf5b5d531095b880ec60294aaa9818757c2a60e
Shivakumar <bashamonishivakumar081@gmail.com>
2026-01-28 21:18:51 +0530

    problem

---
+++ problems.js
@@ -0,0 +1,5 @@
+function submitForm() {
+  const name = document.getElementById("name").value;
+  const surname = document.getElementById("surname").value;
+  console.log("welcome " + name + " " + surname);
+}
