e52abbe46dc8477802f75e2cc1e688faed268707
Shivakumar <bashamonishivakumar081@gmail.com>
2026-02-01 21:52:21 +0530

    Add getNumber and calculateSum functions to handle number input and sum calculation

--- problems.js
+++ problems.js
@@ -40,3 +40,15 @@ function clearInput() {
   document.getElementById("numberInput").value = "";
   document.getElementById("result").innerText = "Result will appear here";
 }
+function getNumber() {
+  return Number(document.getElementById("numberInput").value);
+}
+
+function calculateSum() {
+  let n = getNumber();
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  document.getElementById("result").innerText = "Sum = " + sum;
+}
