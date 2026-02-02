97c5508347323b5536699d071a72db3c341eb299
Shivakumar <bashamonishivakumar081@gmail.com>
2026-02-02 22:29:45 +0530

    Add submitForm function to handle user input and display results

--- script1.js
+++ script1.js
@@ -67,3 +67,30 @@ function clearInput() {
   document.getElementById("numberInput").value = "";
   document.getElementById("result").innerText = "Result will appear here";
 }
+function submitForm() {
+  const name = document.getElementById("name").value;
+  const surname = document.getElementById("surname").value;
+  const fullName = name + " " + surname;
+
+  document.getElementById("output").innerText = " welcome  " + fullName;
+  const word = document.getElementById("word").value;
+  document.getElementById("wordLength").innerText =
+    "Length of the word is: " + word.length;
+}
+const lowercaseWord = "camelcase";
+const camelCasedVersion =
+  lowercaseWord.slice(0, 5) + lowercaseWord[5].toUpperCase();
+console.log("Camel cased version:");
+console.log(camelCasedVersion);
+function checkArmstrong() {
+  let n = getNumber();
+  let temp = n;
+  let sum = 0;
+  while (temp > 0) {
+    let digit = temp % 10;
+    sum += digit ** 3;
+    temp = Math.floor(temp / 10);
+  }
+  document.getElementById("result").innerText =
+    sum === n ? "Armstrong Number" : "Not an Armstrong Number";
+}
