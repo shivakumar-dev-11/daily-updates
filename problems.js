b322e687309ba333021d31df82db591634c694ae
Shivakumar <bashamonishivakumar081@gmail.com>
2026-02-10 22:16:04 +0530

    Refactor number checking functionality to determine even or odd status for three numbers

--- problems.js
+++ problems.js
@@ -1,229 +1,25 @@
-function submitForm() {
-  const name = document.getElementById("name").value;
-  const surname = document.getElementById("surname").value;
-  const fullName = name + " " + surname;
+//day 1 for problem solving
+// finding even or odd number
+function Numbers(num1, num2, num3) {
+  let result = {};
 
-  document.getElementById("output").innerText = " welcome  " + fullName;
-  const word = document.getElementById("word").value;
-  document.getElementById("wordLength").innerText =
-    "Length of the word is: " + word.length;
-}
-const lowercaseWord = "camelcase";
-const camelCasedVersion =
-  lowercaseWord.slice(0, 5) + lowercaseWord[5].toUpperCase();
-console.log("Camel cased version:");
-console.log(camelCasedVersion);
-function checkArmstrong() {
-  let n = getNumber();
-  let temp = n;
-  let sum = 0;
-  while (temp > 0) {
-    let digit = temp % 10;
-    sum += digit ** 3;
-    temp = Math.floor(temp / 10);
-  }
-  document.getElementById("result").innerText =
-    sum === n ? "Armstrong Number" : "Not an Armstrong Number";
-}
-function checkPalindrome() {
-  let n = getNumber();
-  let rev = 0,
-    temp = n;
-  while (temp > 0) {
-    rev = rev * 10 + (temp % 10);
-    temp = Math.floor(temp / 10);
-  }
-  document.getElementById("result").innerText =
-    rev === n ? "Palindrome Number" : "Not a Palindrome";
-}
-function clearInput() {
-  document.getElementById("numberInput").value = "";
-  document.getElementById("result").innerText = "Result will appear here";
-}
-function getNumber() {
-  return Number(document.getElementById("numberInput").value);
-}
-
-function calculateSum() {
-  let n = getNumber();
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    sum += i;
-  }
-  document.getElementById("result").innerText = "Sum = " + sum;
-}
-function checkEven() {
-  let n = getNumber();
-  document.getElementById("result").innerText =
-    n % 2 === 0 ? "Even Number" : "Not Even";
-}
-function checkPalindrome() {
-  let n = getNumber();
-  let rev = 0,
-    temp = n;
-  while (temp > 0) {
-    rev = rev * 10 + (temp % 10);
-    temp = Math.floor(temp / 10);
-  }
-  document.getElementById("result").innerText =
-    rev === n ? "Palindrome Number" : "Not a Palindrome";
-}
-function clearInput() {
-  document.getElementById("numberInput").value = "";
-  document.getElementById("result").innerText = "Result will appear here";
-}
-function getNumber() {
-  return Number(document.getElementById("numberInput").value);
-}
-
-function calculateSum() {
-  let n = getNumber();
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    sum += i;
-  }
-  document.getElementById("result").innerText = "Sum = " + sum;
-}
-function checkEven() {
-  let n = getNumber();
-  document.getElementById("result").innerText =
-    n % 2 === 0 ? "Even Number" : "Not Even";
-}
-function checkEven() {
-  let n = getNumber();
-  document.getElementById("result").innerText =
-    n % 2 === 0 ? "Even Number" : "Not Even";
-}
-const firstResult = 5 + "10";
-console.log(`5 + 10 = ${firstResult}`);
-
-const secondResult = "Eight" - 5;
-console.log(`8 - 5 = ${secondResult}`);
-
-const thirdResult = true + 5;
-console.log(`1 + 5 = ${thirdResult}`);
-
-const fourthResult = false + 8;
-console.log(`0 + 8 = ${fourthResult}`);
-
-const fifthResult = "10" * 2;
-console.log(`10 * 2 = ${fifthResult}`);
-
-const sixthResult = null + 22;
-console.log(`0 + 22 = ${sixthResult}`);
-const hasDeveloperJob = true;
-
-if (hasDeveloperJob) {
-  console.log("Timmy is employed as a developer.");
-}
-
-const isTimmyAGamer = false;
-
-if (isTimmyAGamer) {
-  console.log("Timmy loves to play World of Warcraft.");
-}
-
-const timmyAge = 18;
-
-if (timmyAge >= 16) {
-  console.log("Timmy is old enough to drive.");
-} else {
-  console.log("Timmy is not old enough to drive.");
-}
-const botName = "MathBot";
-const greeting = `Hi there! My name is ${botName} and I am here to teach you about the Math object!`;
-
-console.log(greeting);
-
-console.log(
-  "The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.",
-);
-
-const randomNum = Math.random();
-console.log(randomNum);
-
-console.log("Now, generate a random number between two values.");
-
-const min = 1;
-const max = 100;
-
-const randomNum2 = Math.random() * (max - min) + min;
-console.log(randomNum2);
-
-console.log(
-  "The Math.floor() method rounds the value down to the nearest whole integer.",
-);
-
-const numRoundedDown = Math.floor(6.7);
-console.log(numRoundedDown);
-
-console.log("Now, generate a random integer between two values.");
-
-const randomInt = Math.floor(Math.random() * (max - min) + min);
-console.log(randomInt);
-
-console.log(
-  "The Math.ceil() method rounds the value up to the nearest whole integer.",
-);
-
-const numRoundedUp = Math.ceil(3.2);
-console.log(numRoundedUp);
-
-console.log(
-  "The Math.round() method rounds the value to the nearest whole integer.",
-);
-
-const maxNum = Math.max(3, 125, 55, 24);
-console.log(maxNum);
-const minNum = Math.min(6, 90, 14, 90, 2);
-console.log(minNum);
-const hasDeveloperJob = true;
-
-if (hasDeveloperJob) {
-  console.log("Timmy is employed as a developer.");
-}
-
-const isTimmyAGamer = false;
-
-if (isTimmyAGamer) {
-  console.log("Timmy loves to play World of Warcraft.");
-}
-
-console.log(
-  "The Math.max() and Math.min() methods are used to get the maximum and minimum number from a range.",
-);
-
-if (timmyAge >= 16) {
-  console.log("Timmy is old enough to drive.");
-} else {
-  console.log("Timmy is not old enough to drive.");
-}
-const minIncomeForDuplex = 60000;
-const minCreditScoreForDuplex = 700;
-
-const minIncomeForCondo = 45000;
-const minCreditScoreForCondo = 680;
-
-const minIncomeForCar = 30000;
-const minCreditScoreForCar = 650;
-
-function getLoanMessage(annualIncome, creditScore) {
-  if (
-    creditScore >= minCreditScoreForDuplex &&
-    annualIncome >= minIncomeForDuplex
-  ) {
-    return "You qualify for a duplex, condo, and car loan.";
-  } else if (
-    annualIncome >= minIncomeForCondo &&
-    creditScore >= minCreditScoreForCondo
-  ) {
-    return "You qualify for a condo and car loan.";
-  } else if (
-    annualIncome >= minIncomeForCar &&
-    creditScore >= minCreditScoreForCar
-  ) {
-    return "You qualify for a car loan.";
+  if (num1 % 2 === 0) {
+    result.num1 = "even number";
   } else {
-    return "You don't qualify for any loans.";
+    result.num1 = "odd number";
   }
+
+  if (num2 % 2 === 0) {
+    result.num2 = "even number";
+  } else {
+    result.num2 = "odd number";
+  }
+
+  if (num3 % 2 === 0) {
+    result.num3 = "even number";
+  } else {
+    result.num3 = "odd number";
+  }
+
+  return result;
 }
