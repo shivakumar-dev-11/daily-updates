9fade5d47fc1528872ce44bafe2fa4cc72e0eb14
Shivakumar <bashamonishivakumar081@gmail.com>
2026-02-12 23:40:22 +0530

    Remove vowelsCount function implementation

--- problems.js
+++ problems.js
@@ -47,14 +47,3 @@ function reverseString(str) {
   return str.split("").reverse().join("");
 }
 // count the number of vowels in a string
-function vowelsCount(str) {
-  let count = 0;
-  let vowels = "AEIOUaeiou";
-  for (let char of str) {
-    if (vowels.includes(char)) {
-      count++;
-    }
-  }
-
-  return count;
-}
