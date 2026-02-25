23311a61fb68e58e84b2b7f6b0649ff6fdd60997
Shivakumar <bashamonishivakumar081@gmail.com>
2026-02-25 23:38:25 +0530

    git-last

--- script1.js
+++ script1.js
@@ -176,7 +176,3 @@ console.log(minNum);
 console.log("Grocery shopping list");
 
 const shoppingList = [];
-
-console.log("It will be nice to have some fruit to eat.");
-
-shoppingList.push("Apples");
