127b3e9edf3773c061b45ee601ec185c61f66813
Shivakumar <bashamonishivakumar081@gmail.com>
2026-03-01 23:10:09 +0530

    Remove redundant console logs for rounded numbers in script1.js

--- script1.js
+++ script1.js
@@ -149,8 +149,3 @@ console.log(numRoundedUp);
 console.log(
   "The Math.round() method rounds the value to the nearest whole integer.",
 );
-
-const numRounded = Math.round(2.7);
-console.log(numRounded);
-const numRounded2 = Math.round(11.2);
-console.log(numRounded2);
