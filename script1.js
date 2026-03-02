5aa75149b0055fe95973c0f2854f5b16041997f9
Shivakumar <bashamonishivakumar081@gmail.com>
2026-03-02 20:20:44 +0530

    Remove console log for Math.ceil() method description in script1.js

--- script1.js
+++ script1.js
@@ -138,7 +138,3 @@ console.log("Now, generate a random integer between two values.");
 
 const randomInt = Math.floor(Math.random() * (max - min) + min);
 console.log(randomInt);
-
-console.log(
-  "The Math.ceil() method rounds the value up to the nearest whole integer.",
-);
