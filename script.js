b2a5fb29d0748364d5ae90770959bc96ad97e1b7
Shivakumar <bashamonishivakumar081@gmail.com>
2026-03-13 23:15:05 +0530

    const

--- script.js
+++ script.js
@@ -206,11 +206,3 @@ console.log(trimmedStart);
 const trimmedEnd = userInput.trimEnd();
 console.log("After using the trimEnd() method, trailing spaces removed:");
 console.log(trimmedEnd);
-
-const upperCaseInput = cleanedInput.toUpperCase();
-console.log("Result of using the toUpperCase() method:");
-console.log(upperCaseInput);
-
-const lowerCaseInput = cleanedInput.toLowerCase();
-console.log("Result of using the toLowerCase() method:");
-console.log(lowerCaseInput);
