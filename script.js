4a608ad5e9f2d09f23cc8300d6559029aedf0cc7
Shivakumar <bashamonishivakumar081@gmail.com>
2026-03-14 23:36:23 +0530

    twocommit

--- script.js
+++ script.js
@@ -198,7 +198,3 @@ console.log(userInput);
 const cleanedInput = userInput.trim();
 console.log("Result of trimming whitespace from both ends:");
 console.log(cleanedInput);
-
-const trimmedStart = userInput.trimStart();
-console.log("After using the trimStart() method, leading spaces removed:");
-console.log(trimmedStart);
