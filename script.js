da8232ab9921fddace998aa0c98422d6de64bafc
Shivakumar <bashamonishivakumar081@gmail.com>
2026-03-19 21:57:21 +0530

    commit

--- script.js
+++ script.js
@@ -169,11 +169,3 @@ console.log(favoriteSubjectSentence);
 const originalString = "I love cats.";
 console.log("Original string:");
 console.log(originalString);
-
-const replacedString = originalString.replace("cats", "dogs");
-console.log("After using the replace() method:");
-console.log(replacedString);
-
-const exampleSentence = "I love cats and cats are so much fun!";
-console.log("Original sentence:");
-console.log(exampleSentence);
