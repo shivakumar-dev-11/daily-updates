b743b419c989fdf88e15915c942552f7a730678c
Shivakumar <bashamonishivakumar081@gmail.com>
2026-04-04 21:49:26 +0530

    one change

--- script.js
+++ script.js
@@ -90,9 +90,3 @@ const botName = "teacherBot";
 
 const greeting = `My name is ${botName}.`;
 console.log(greeting);
-
-const subject = "JavaScript";
-const topic = "strings";
-
-const sentence = `Today, you will learn about ${topic} in ${subject}.`;
-console.log(sentence);
