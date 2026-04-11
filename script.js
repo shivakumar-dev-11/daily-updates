4f377aa2ffd55655c18ea10cf0618ac9e0cf7385
Shivakumar <bashamonishivakumar081@gmail.com>
2026-04-11 21:19:24 +0530

    1script

--- script.js
+++ script.js
@@ -61,8 +61,3 @@ const hasFreeCodeCamp = fccSentence.includes("freeCodeCamp");
 console.log(
   `fccSentence.includes("freeCodeCamp") returns ${hasFreeCodeCamp} because the word "freeCodeCamp" is in the sentence.`,
 );
-
-const hasJavaScript = fccSentence.includes("JavaScript");
-console.log(
-  `fccSentence.includes("JavaScript") returns ${hasJavaScript} because the word "JavaScript" is not in the sentence.`,
-);
