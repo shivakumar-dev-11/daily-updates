f578ed623525397714c540a9280220d546943735
Shivakumar <bashamonishivakumar081@gmail.com>
2026-04-12 22:33:00 +0530

    last2

--- script.js
+++ script.js
@@ -56,8 +56,3 @@ console.log(trimmedEnd);
 const fccSentence = "freeCodeCamp is a great place to learn web development.";
 
 console.log("Here are some examples of the includes() method:");
-
-const hasFreeCodeCamp = fccSentence.includes("freeCodeCamp");
-console.log(
-  `fccSentence.includes("freeCodeCamp") returns ${hasFreeCodeCamp} because the word "freeCodeCamp" is in the sentence.`,
-);
